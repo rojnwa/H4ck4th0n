a186d88b346e87ff0f287891fc51f783b1d97df7
Roman Wagner <roman.wagner@pm.me>
2020-01-10 21:20:55 +0100

    Player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D rb2D;
+    private int speed = 10;
+    private int jumpForce = 10;
+    private enum Grounded { Resting, Jumping };
+    private Grounded isGrounded = Grounded.Resting;
+
+
     void Start()
     {
+        rb2D = GetComponent<Rigidbody2D>();
 
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        if (Input.GetButton("Left"))
+        {
+            rb2D.transform.Translate(-transform.right * speed * Time.fixedDeltaTime);
+        }
+
+        if (Input.GetButton("Right"))
+        {
+            rb2D.transform.Translate(transform.right * speed * Time.fixedDeltaTime);
+        }
+
+        if (Input.GetButton("Jump") && (isGrounded == Grounded.Resting))
+        {
+            rb2D.transform.Translate(transform.up * jumpForce * Time.fixedDeltaTime);
+        }
 
     }
+
 }
