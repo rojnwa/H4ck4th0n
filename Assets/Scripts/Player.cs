d79b7db5d7dc6a78c28963b0b287e5ee5d7175c9
Roman Wagner <roman.wagner@pm.me>
2020-01-11 00:31:05 +0100

    Jump Grounding

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb2D;
+    private Collider2D col2D;
     private int speed = 10;
-    private int jumpForce = 10;
+    private int jumpForce = 7;
     private enum Grounded { Resting, Jumping, DoubleJumped };
     private Grounded isGrounded = Grounded.Resting;
     private bool doubleJumpUpgradeAcquired;
     private bool sprintUpgradeUpgradeAcquired;
+    private float distToGround;
 
 
     void Start()
     {
+        col2D = GetComponent<Collider2D>();
         rb2D = GetComponent<Rigidbody2D>();
+        distToGround = col2D.bounds.extents.y;
+    }
 
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.GetComponent<GroundScript>())
+        {
+            isGrounded = Grounded.Resting;
+        }
     }
 
     void Update()
@@ -36,8 +47,9 @@ public class Player : MonoBehaviour
             rb2D.transform.Translate(transform.right * speed * Time.fixedDeltaTime);
         }
 
-        if (Input.GetButtonDown("Sprint") && sprintUpgradeUpgradeAcquired)
+        if (Input.GetButton("Sprint") && sprintUpgradeUpgradeAcquired)
         {
+            Debug.Log(Input.GetButtonDown("Sprint"));
             speed = 20;
         }
         else
@@ -45,15 +57,15 @@ public class Player : MonoBehaviour
             speed = 10;
         }
 
-        if (Input.GetButton("Jump") && (isGrounded == Grounded.Resting))
+        if (Input.GetButtonDown("Jump") && (isGrounded == Grounded.Resting))
         {
             isGrounded = Grounded.Jumping;
-            rb2D.transform.Translate(transform.up * jumpForce * Time.fixedDeltaTime);
+            rb2D.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
 
-            if (doubleJumpUpgradeAcquired && (isGrounded != Grounded.DoubleJumped) && Input.GetButton("Jump"))
+            if (doubleJumpUpgradeAcquired && (isGrounded != Grounded.DoubleJumped) && Input.GetButtonDown("Jump"))
             {
                 isGrounded = Grounded.DoubleJumped;
-                rb2D.transform.Translate(transform.up * jumpForce * Time.fixedDeltaTime);
+                rb2D.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
             }
         }
 
