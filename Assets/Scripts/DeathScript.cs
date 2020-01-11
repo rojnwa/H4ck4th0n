af47287ae0ab30e2f0dcf2c22a18c2c189eeddbe
Qryper <simon-schmieden@web.de>
2020-01-11 12:55:30 +0100

    sw

---
+++ Assets/Scripts/DeathScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<Player>())
+        {
+            collision.gameObject.SendMessage("GetDamage", 1f);
+        }
+    }
+}
