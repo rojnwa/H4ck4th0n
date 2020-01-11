97ac3962d6acc5c1ee58d554085c853abd4d4e3c
Roman Wagner <roman.wagner@pm.me>
2020-01-11 01:31:49 +0100

    Drop

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,69 +2,74 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
     private Rigidbody2D rb2D;
     private Collider2D col2D;
-    private int speed = 10;
+    private int speed = 5;
     private int jumpForce = 7;
     private enum Grounded { Resting, Jumping, DoubleJumped };
     private Grounded isGrounded = Grounded.Resting;
     private bool doubleJumpUpgradeAcquired;
     private bool sprintUpgradeUpgradeAcquired;
     private float distToGround;
+    private bool dropPressed;
 
-
-    void Start()
-    {
+    void Start() {
         col2D = GetComponent<Collider2D>();
         rb2D = GetComponent<Rigidbody2D>();
         distToGround = col2D.bounds.extents.y;
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.GetComponent<GroundScript>())
-        {
-            isGrounded = Grounded.Resting;
-        }
-    }
 
-    void Update()
-    {
+    void Update() {
 
     }
 
-    void FixedUpdate()
-    {
-        if (Input.GetButton("Left"))
-        {
+
+    void FixedUpdate() {
+
+        if (Input.GetButton("Left")) {
             rb2D.transform.Translate(-transform.right * speed * Time.fixedDeltaTime);
         }
 
-        if (Input.GetButton("Right"))
-        {
+        if (Input.GetButton("Right")) {
             rb2D.transform.Translate(transform.right * speed * Time.fixedDeltaTime);
         }
 
-        speed = (Input.GetButton("Sprint") && sprintUpgradeUpgradeAcquired) ? 20 : 10;
-
-        if (Input.GetButtonDown("Jump") && (isGrounded == Grounded.Resting))
-        {
+        if (Input.GetButtonDown("Jump") && (isGrounded == Grounded.Resting)) {
             isGrounded = Grounded.Jumping;
             rb2D.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
 
-            if (doubleJumpUpgradeAcquired && (isGrounded != Grounded.DoubleJumped) && Input.GetButtonDown("Jump"))
-            {
+            if (doubleJumpUpgradeAcquired && (isGrounded != Grounded.DoubleJumped) && Input.GetButtonDown("Jump")) {
                 isGrounded = Grounded.DoubleJumped;
                 rb2D.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
             }
         }
 
+        speed *= (Input.GetButton("Sprint") && sprintUpgradeUpgradeAcquired) ? 2 : 1;
+
     }
 
-    void resetGrounded()
-    {
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.GetComponent<GroundScript>()) {
+            isGrounded = Grounded.Resting;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D other) {
+        if (Input.GetButtonDown("Down") && other.gameObject.GetComponent<Dropable>()) {
+            DropThrough();
+            Invoke("DropThrough", 0.5f);
+        }
+
+    }
+
+    void DropThrough() {
+        col2D.enabled = !col2D.enabled;
+    }
+
+    void resetGrounded() {
         isGrounded = Grounded.Resting;
     }
 
