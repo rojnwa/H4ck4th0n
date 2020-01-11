cc5b959839a61b8ee4b2d54bcf20ab3741260ceb
Leon Koth <leon.koth97@gmail.com>
2020-01-11 08:59:51 +0100

    enemy update

--- Assets/Scripts/Fox.cs
+++ Assets/Scripts/Fox.cs
@@ -27,7 +27,7 @@ public class Fox : MonoBehaviour
 
     private bool playerIsVisible;
 
-    private bool triggeredWalking, inCooldown, doJumpAttack;
+    private bool triggeredWalking, inCooldown, doJumpAttack, walking, inCd;
 
     private Random random;
 
@@ -39,19 +39,54 @@ public class Fox : MonoBehaviour
         hitBox = GetComponent<BoxCollider2D>();
         rb2D = GetComponent<Rigidbody2D>();
         velocity = new Vector2(1, 0);
-        playerIsVisible = false;
         inCooldown = false;
         triggeredWalking = false;
-        doJumpAttack = false;
+        inCd = false;
         if (dir == Direction.Left)
             transform.localScale = new Vector2(-1, 1);
         random = new Random();
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<Player>() == null && other.gameObject.GetComponent<Sword>() == null)
+        {
+            Debug.Log("Change");
+                if (dir == Direction.Left)
+                {
+                    dir = Direction.Right;
+                    transform.localScale = new Vector2(1, 1);
+                Debug.Log("Rotate");
+                }
+                else
+                {
+                    dir = Direction.Left;
+                    transform.localScale = new Vector2(-1, 1);
+                }
+        }
+        else if (!inCooldown)
+        {
+            animator.Play("Fox - Attack " + (int)Random.Range(1,3));
+            triggeredWalking = false;
+            StartCoroutine(MovementCooldown());
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.GetComponent<Player>() != null && !inCooldown)
+        {
+            animator.Play("Fox - Attack " + (int)Random.Range(1, 3));
+            triggeredWalking = false;
+            StartCoroutine(MovementCooldown());
+        }
+    }
+
+
     // Update is called once per frame
     void Update()
     {
-        if (health <= 0)
+        if(health <= 0)
         {
             if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Fox - Death"))
             {
@@ -59,101 +94,25 @@ public class Fox : MonoBehaviour
                 StartCoroutine(DeathCountdown());
             }
         }
-        else {
-
-            Collider2D[] vHits = Physics2D.OverlapBoxAll(transform.position, visBox.size, 0);
-            foreach (Collider2D hit in vHits)
-            {
-
-                if (hit.gameObject.GetComponent<Player>() != null)
-                {
-                    playerIsVisible = true;
-                    if (Vector2.Distance(transform.position, hit.gameObject.transform.position) < 4.6 && Vector2.Distance(transform.position, hit.gameObject.transform.position) > 4.5 && Random.value < 0.1)
-                    {
-                        doJumpAttack = true;
-                        Debug.Log("Jump");
-                    }
-                }
-            }
-
-            if (!inCooldown && playerIsVisible && !triggeredWalking)
-                TriggerWalking();
-
-            if (doJumpAttack)
-            {
-                animator.Play("Fox - Attack 2");
-                if (dir == Direction.Left)
-                {
-                    transform.position = new Vector2(transform.position.x - 2, transform.position.y);
-                }
-                else
-                {
-                    transform.position = new Vector2(transform.position.x + 2, transform.position.y);
-                }
-
-
-                triggeredWalking = false;
-                inCooldown = true;
-                doJumpAttack = false;
-                StartCoroutine(MovementCooldown());
-            }
-            else
-            if (!inCooldown && playerIsVisible && triggeredWalking && (!animator.GetCurrentAnimatorStateInfo(0).IsName("Fox - Attack 1") && !animator.GetCurrentAnimatorStateInfo(0).IsName("Fox - Attack 2")))
-            {
-
-                if (dir == Direction.Left)
-                {
-                    velocity.x = -1;
-                }
-                else
-                {
-                    velocity.x = 1;
-                }
-
-                transform.Translate(velocity * Time.deltaTime);
-
-                Collider2D[] hits = Physics2D.OverlapBoxAll(transform.position, hitBox.size, 0);
-
-                foreach (Collider2D hit in hits)
-                {
-                    if (hit == hitBox || hit == visBox)
-                        continue;
-
-                    ColliderDistance2D colliderDistance = hit.Distance(hitBox);
-
-                    if (colliderDistance.isOverlapped)
-                    {
-                        Vector2 v = colliderDistance.pointA - colliderDistance.pointB;
-
-                        if (hit.gameObject.GetComponent<Player>() == null)
-                        {
-                            if (v.x != 0)
-                            {
-                                if (dir == Direction.Left)
-                                {
-                                    dir = Direction.Right;
-                                    transform.localScale = new Vector2(1, 1);
-                                }
-                                else
-                                {
-                                    dir = Direction.Left;
-                                    transform.localScale = new Vector2(-1, 1);
-                                }
-                            }
-                        }
-                        else
-                        {
-                            animator.Play("Fox - Attack 1");
-                            triggeredWalking = false;
-                            inCooldown = true;
-                            StartCoroutine(MovementCooldown());
-                        }
-                        transform.Translate(v);
-
-                    }
-                }
-            }
+        if (dir == Direction.Left)
+        {
+            velocity.x = -1;
         }
+        else
+        {
+            velocity.x = 1;
+        }
+
+        if(!walking || !triggeredWalking)
+            velocity.x = 0;
+
+        transform.Translate(velocity * Time.deltaTime);
+
+        if (!inCd)
+        {
+            StartCoroutine(WalkingCountdown());
+        }
+
     }
 
     private void TriggerWalking()
@@ -164,8 +123,8 @@ public class Fox : MonoBehaviour
 
     private IEnumerator MovementCooldown()
     {
-
-        float duration = 3f;
+        inCooldown = true;
+        float duration = 2f;
         float normalizedTime = 0;
         while (normalizedTime <= 1f)
         {
@@ -175,6 +134,31 @@ public class Fox : MonoBehaviour
         inCooldown = false;
         TriggerWalking();
 
+    }
+
+    private IEnumerator WalkingCountdown()
+    {
+        inCd = true;
+        float duration = 2f;
+        if (walking)
+            duration = duration * 3;
+        float normalizedTime = 0;
+        while (normalizedTime <= 1f)
+        {
+            normalizedTime += Time.deltaTime / duration;
+            yield return null;
+        }
+        walking = !walking;
+        if (walking)
+        {
+            TriggerWalking();
+        }
+        else
+        {
+            animator.Play("Fox - Idle");
+        }
+
+        inCd = false;
     }
 
     private IEnumerator DeathCountdown()
