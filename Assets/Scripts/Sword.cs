42675268752bf14c5918b8cd265fb46e2459f5fa
Roman Wagner <roman.wagner@pm.me>
2020-01-11 05:02:07 +0100

    Attack

---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour {
+
+    private Collider2D col2D;
+    private float currentDamage;
+
+    void Start() {
+        col2D = GetComponent<Collider2D>();
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.GetComponent<Hittable>()) {
+            other.gameObject.SendMessage("Hurt", currentDamage);
+        }
+    }
+}
